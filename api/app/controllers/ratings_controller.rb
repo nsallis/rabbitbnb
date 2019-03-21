850a0ed30e6604b7a9eb2e572f22bbafb0bd5db9
nsallis <nsallis@enservio.com>
2019-03-21 15:38:00 -0400

    cleanup and generic error handling for json

--- api/app/controllers/ratings_controller.rb
+++ api/app/controllers/ratings_controller.rb
@@ -1,23 +1,23 @@
 class RatingsController < ActionController::Base
-skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
+    rescue_from Exception, :with => :generic_json_error
+
     def create
-        f_name = params[:voter]["first_name"]
-        l_name = params[:voter]["last_name"]
+        f_name = params[:voter]['first_name'] # we should probably be using strong params
+        l_name = params[:voter]['last_name']
 
         # check if the voter has already voted
         user_vote = UserVote.where(first_name: f_name, last_name: l_name)
-        render json: {error: "you have already voted"} if user_vote.any?
+        render json: {error: 'you have already voted'} if user_vote.any?
         return if user_vote.any?
 
-        rating = Rating.where(name: params[:name]).first_or_create
-        rating.votes += 1
-        rating.voters.push("#{f_name} #{l_name}")
-        rating.save!
+        # add a vote to the rating
+        Rating.vote(params[:name], f_name, l_name)
 
         # register that the user has now voted
-        user_vote = UserVote.create(first_name: f_name, last_name: l_name, voted_for: params[:name])
-        user_vote.save
-        render json: {status: "ok"}.to_json
+        UserVote.create!(first_name: f_name, last_name: l_name, voted_for: params[:name])
+
+        render json: {status: 'ok'}.to_json
     end
 
     def index
@@ -26,6 +26,11 @@ skip_before_action :verify_authenticity_token
 
     def show
         render json: Rating.where(name: params[:name]).first.to_json
+    end
+
+    private
+    def generic_json_error(error)
+        render json: {error: error.message}
     end
 end
 
--- api/app/models/rating.rb
+++ api/app/models/rating.rb
@@ -1,2 +1,9 @@
 class Rating < ApplicationRecord
+
+    def self.vote(name, f_name, l_name)
+        rating = Rating.where(name: name).first_or_create
+        rating.votes += 1
+        rating.voters.push("#{f_name} #{l_name}")
+        rating.save!
+    end
 end
